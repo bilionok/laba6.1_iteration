05cb02af480596e591f247ab407ba1f85098ceaa
bilionok <photoshoper16@gmail.com>
2020-12-17 10:46:35 +0200

    n

---
+++ lab6.1/lab6.1.cpp
@@ -0,0 +1,54 @@
+﻿// 6.1.cpp 
+#include <iostream>
+#include<iomanip>
+
+using namespace std;
+
+void format_mas(int* a, int& sum, int& k, const int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if ((a[i] > 0) && !(i % 5 == 0))
+        {
+            k++;
+            sum += a[i];
+            a[i] = 0;
+        }
+    }
+    cout << '\n';
+    cout << "quantity - " << k << '\n';
+    cout << "sum - " << sum << '\n';
+    cout << '\n';
+}
+
+void print(int* a, const int n)
+{
+    cout << setw(3) << "a[] = ";
+    for (int i = 0; i < n; i++)
+        cout << setw(4) << a[i] << ",";
+    cout << '\n';
+}
+
+void create(int* a, const int n, const int Low, const int High)
+{
+    srand(unsigned(time(0)));
+
+    for (int i = 0; i < n; i++)
+        a[i] = Low + rand() % (High - Low + 1);
+}
+
+int main()
+{
+    const int n = 20;
+    int a[n];
+
+
+    create(a, n, -20, 50); 
+    print(a, n);
+    int sum = 0;
+    int k = 0;
+    format_mas(a, sum, k, n);  
+    print(a, n);
+
+    getchar();
+}
